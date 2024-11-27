7127bb9afa43005a4c25fd893c5826d30b7b1144
Marat Khabibullin <140619138+HabibullinMarat@users.noreply.github.com>
2024-11-27 16:54:04 +0500

    Task1

---
+++ Tyuiu.KhabibullinMR.Sprint4.Task1.V15.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KhabibullinMR.Sprint4.Task1.V15.Lib
+{
+    public class DataService : ISprint4Task1V15
+    {
+        public int Calculate(int[] array)
+        {
+            int haha = 1;
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    haha*=array[i];
+                }
+            }
+            return haha;
+        }
+    }
+}
---
+++ Tyuiu.KhabibullinMR.Sprint4.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.KhabibullinMR.Sprint4.Task1.V15.Lib;
+
+namespace Tyuiu.KhabibullinMR.Sprint4.Task1.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[] array = new int[] { 6, 7, 6, 7, 5, 7, 6, 7, 9, 4, 6, 5, 8, 5 };
+            int wait = 41472;
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
